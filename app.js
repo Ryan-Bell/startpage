df32fdadf94f0355b6b541442d08d4b458a1bc54
Ryan Bell <rjb1928@rit.edu>
2019-02-22 12:15:15 -0500

    added simple weather output

--- app.js
+++ app.js
@@ -28,6 +28,26 @@ const onRequest = (request, response) => {
         sendResponse(aggregateData, 'text/plain');
       });
       break;
+    case '/weather':
+      (function(){
+        const child = spawn('/home/mainuser/go/bin/weather', ['--server', 'http://morganfreeman.rit.edu:1234']);
+        let aggregateData = '';
+        child.on('error', (data) => {
+          console.log('error');
+          console.log(data);
+        });
+        child.stdout.on('data', (data) => {
+          aggregateData += data;
+        });
+        child.stderr.on('data', (data) => {
+          aggregateData += data;
+        });
+
+        child.on('exit', (code, signal) => {
+          sendResponse(aggregateData, 'text/plain');
+        });
+      })();
+      break;
     case '/style.css':
       style = fs.readFileSync(`${__dirname}/style.css`);
       sendResponse(style, 'text/css');
