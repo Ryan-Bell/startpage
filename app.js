7ecd305f63f62b41bda5e669e623374089668d7a
Ryan Bell <rjb1928@rit.edu>
2019-03-03 17:49:29 -0500

    slight refactoring, 404, add item poc

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http = require('http');
 const fs = require('fs');
 const { spawn } = require('child_process');
+
+// setup the converter for ansi color sequences
 const ansiConverter = require('ansi-to-html');
 let converter = new ansiConverter();
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+// index and style files
 let index, style;
 
 const onRequest = (request, response) => {
@@ -17,10 +20,39 @@ const onRequest = (request, response) => {
     response.end();
   }
 
+  const handleTasks = () => {
+    const child = spawn('/home/mainuser/bin/t', ['--list', 'startpage']);
+    let aggregateData = '';
+    child.stdout.on('data', (data) => {
+      aggregateData += data;
+    });
+
+    child.on('exit', (code, signal) => {
+      sendResponse(aggregateData, 'text/plain');
+    });
+  };
+
+  const handleWeather = () => {
+    const child = spawn('/home/mainuser/go/bin/weather', ['--server', 'http://morganfreeman.rit.edu:1234', '-ignore-alerts']);
+    let aggregateData = '';
+    child.on('error', (data) => {
+      console.log(data);
+    });
+    child.stdout.on('data', (data) => {
+      aggregateData += data;
+    });
+    child.stderr.on('data', (data) => {
+      aggregateData += data;
+    });
+
+    child.on('exit', (code, signal) => {
+      sendResponse(converter.toHtml(aggregateData.toString()), 'text/plain');
+    });
+  };
+
   console.log(request.url);
-  //TODO docker, tasks list, plex information, simple auth system but I'm always logged in on local machine
-  //TODO remove github repos that are always private like old plex website
-  //endpoint to get colors set on computer so that they can be applied in css
+  //TODO docker, tasks list, plex information, simple auth system but stay logged in on local machine
+  //TODO endpoint to get colors set on computer so that they can be applied in css
   switch (request.url) {
     case '/uptime':
       const child = spawn('uptime');
@@ -34,37 +66,10 @@ const onRequest = (request, response) => {
       });
       break;
     case '/weather':
-      (function(){
-        const child = spawn('/home/mainuser/go/bin/weather', ['--server', 'http://morganfreeman.rit.edu:1234', '-ignore-alerts']);
-        let aggregateData = '';
-        child.on('error', (data) => {
-          console.log('error');
-          console.log(data);
-        });
-        child.stdout.on('data', (data) => {
-          aggregateData += data;
-        });
-        child.stderr.on('data', (data) => {
-          aggregateData += data;
-        });
-
-        child.on('exit', (code, signal) => {
-          sendResponse(converter.toHtml(aggregateData.toString()), 'text/plain');
-        });
-      })();
+      handleWeather();
       break;
     case '/tasks':
-      (function(){
-      const child = spawn('/home/mainuser/bin/t');
-      let aggregateData = '';
-      child.stdout.on('data', (data) => {
-        aggregateData += data;
-      });
-
-      child.on('exit', (code, signal) => {
-        sendResponse(aggregateData, 'text/plain');
-      });
-      })();
+      handleTasks();
       break;
     case '/style.css':
       style = fs.readFileSync(`${__dirname}/style.css`);
@@ -72,11 +77,14 @@ const onRequest = (request, response) => {
       break;
     case '/':
     case '/index.html':
-    default:
       index = fs.readFileSync(`${__dirname}/index.html`);
       sendResponse(index, 'text/html');
       break;
+    default:
+      sendResponse('Page Not Found', 'text/plain', 404);
+      break;
   }
 }
+
 
 http.createServer(onRequest).listen(port);
